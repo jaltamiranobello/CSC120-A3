000afbaf52a8a3384159fdbe79ded5b04ceca3b0
jaltamiranobello <jaltamiranobello@smith.edu>
2023-02-15 15:14:16 -0500

    chatbox working wihtout punctuation/capitalization

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,73 @@
+import java.util.*;
 class Conversation {
 
   public static void main(String[] arguments) {
     // You will start the conversation here.
+    System.out.print("How many rounds? ");
+    int rounds;
+    Scanner userInput = new Scanner(System.in);
+    List <String> transcript = new ArrayList<>();
+    transcript.add("Hi there! What's on your mind?");
+    rounds = userInput.nextInt();
+    System.out.println("Hi there! What's on your mind?");
+    for (int i = 0; i < rounds;  i++) {
+      Scanner response = new Scanner(System.in);
+      String userResponse = response.nextLine();
+      String[] mirrorWords = {"I", "me", "Me","Am", "am", "You", "you", "my", "My", "your", "Your"};
+      String [] splitPhrase = userResponse.split(" ");
+      //System.out.println(splitPhrase.getClass().getName());
+      //System.out.println(Arrays.toString(splitPhrase));
+
+      for (String word: splitPhrase) {
+        int index = Arrays.asList(splitPhrase).indexOf(word);
+        if (word.equals(mirrorWords[0])) {
+          splitPhrase[index] = "you";
+        }
+        if (word.equals(mirrorWords[1]) || word.equals(mirrorWords[2])) {
+          splitPhrase[index] = "you";
+        }
+        if (word.equals(mirrorWords[3]) || word.equals(mirrorWords[4])) {
+          splitPhrase[index] = "are";
+        }
+        if (word.equals(mirrorWords[5]) || word.equals(mirrorWords[6])) {
+          splitPhrase[index] = "I";
+        }
+        if (word.equals(mirrorWords[7]) || word.equals(mirrorWords[8])) {
+          splitPhrase[index] = "your";
+        }
+        if (word.equals(mirrorWords[9]) || word.equals(mirrorWords[10])) {
+          splitPhrase[index] = "my";
+        }
+        //System.out.println(splitPhrase);
+      }
+       //for (String word: splitPhrase){
+        // System.out.print(word + " ");
+       String new_sent = String.join(" ", splitPhrase);
+       if (!userResponse.equals(new_sent)) {
+        transcript.add(userResponse);
+        transcript.add(new_sent);
+        System.out.println(new_sent);
+       }
+       //}
+
+      if (userResponse.equals(new_sent)){     
+        String[] replies = {"Intresting!", "Cool!", "Nice!", "Ok."};
+        Random num = new Random();
+        int x = num.nextInt(replies.length);
+        System.out.println(replies[x]);
+        transcript.add(userResponse);
+        transcript.add(replies[x]);
+      }
+             
+
+    }
+    System.out.println("Goodbye!\n");
+    transcript.add("Goodbye!");
+    // System.out.print(transcript);
+    System.out.println("~TRANSCRIPT~");
+    for (int i = 0; i < transcript.size(); i+= 1){
+      System.out.println(transcript.get(i));
+    }
   }
+
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,13 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+Kimberly By Goytia and Taylor Knibb and Kayleigh Castillo
+I did the for loops for rounds and kim did the transcript list and appending 
 - Which session(s) of TA / office hours did you attend?
-- What are your initial impressions of Java? 
-- Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+1-3pm on Sunday 3/12
+7-9 pm Sunday 3/12
+7-9pm on Monday 3/13
+- What are your initial impressions of Java?
+Java is ugly
+- Can you draw any conclusion about programming in general from the similarities or the differences between the two languages?
 - What worked, what didn't, what advice would you give someone taking this course in the future?
